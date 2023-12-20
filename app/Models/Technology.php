8b9c01945ea0f389494d4f4f949730ddf96011ec
Gianmarco Incocciati <minusgian@gmail.com>
2023-12-20 19:11:28 +0100

    add comments

--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class Technology extends Model
 {
     use HasFactory;
-    protected $fillable = ['image','name', 'body'];
+    protected $fillable = ['image', 'name', 'body'];
     public function posts():HasMany
     {
         return $this->hasMany(Post::class);
--- database/migrations/2023_06_07_165930_add_technology_id_to_posts_table.php
+++ database/migrations/2023_06_07_165930_add_technology_id_to_posts_table.php
@@ -27,7 +27,17 @@ return new class extends Migration
     public function down()
     {
         Schema::table('posts', function (Blueprint $table) {
+
+            /*
+            Rimuove la chiave esterna associata alla colonna 'technology_id' nella tabella 'posts'.
+            Il parametro 'posts_technology_id_foreign' è il nome della chiave esterna, che di solito è generato in modo automatico da Laravel, seguendo una convenzione di nomi.
+            Questo comando elimina l'associazione tra la colonna 'technology_id' e la tabella 'technologies'.
+            */
             $table->dropForeign('posts_technology_id_foreign');
+
+            /*
+            Rimuove la colonna 'technology_id' dalla tabella 'posts'. Questo comando elimina fisicamente la colonna dallo schema del database.
+            */
             $table->dropColumn('technology_id');
         });
     }
